6e82ed3811e33b3ed24ff80d7841d29a63dc2d00
Dauba <ab6060118@gmail.com>
2015-05-18 00:57:46 +0800

    modifit createTree.php output json file

--- cgi/createTree.php
+++ cgi/createTree.php
@@ -1,20 +1,22 @@
 <?php
-    $rootPath="../share";
-    $idName="node_";
+    $rootPath="/home/pi/web/910_cloud/share";
     $idCounter="0";
     $tree=Array();
+    $file=fopen("/home/pi/web/910_cloud/tree.json", "w");
 
-    initalTree($tree, $rootPath, $idCounter, $idName);
+    initalTree($tree, $rootPath, $idCounter);
 
     $rootLength=count($tree);
 
     for($i=0; $i<$rootLength; $i++) {
-        findAllChild($tree[$i], $rootPath, $idCounter, $idName, $tree);
+        findAllChild($tree[$i], $rootPath, $idCounter, $rootPath, $tree);
     }
 
-    echo json_encode($tree);
+    // echo json_encode($tree);
+    fwrite($file, json_encode($tree));
+    fclose($file);
 
-    function initalTree(&$tree, $rootPath, &$idCounter, $idName) {
+    function initalTree(&$tree, $rootPath, &$idCounter) {
         $dirArr=Array();
         $dirArr=scandir($rootPath);
 
@@ -22,7 +24,7 @@
 
         foreach($dirArr as $item) {
             $itemArr=array();
-            $itemArr["id"]=$idName.$idCounter++;
+            $itemArr["id"]="node_".$idCounter++;
             $itemArr["text"]=$item;
             $itemArr["parent"]="#";
 
@@ -30,28 +32,30 @@
         }
     }
 
-    function findAllChild(&$node, $path, &$idCounter, $idName, &$treeRoot) {
+    function findAllChild(&$node, $path, &$idCounter, $rootPath, &$treeRoot) {
         $currentPath=$path."/".$node["text"];
 
         if(is_dir($currentPath)) {
             $dirArr=Array();
-            $dirArr=scandir("$currentPath");
+            $dirArr=scandir($currentPath);
             $dirArr=array_slice($dirArr, 2);
 
             foreach($dirArr as $item) {
                 $itemArr=array();
-                $itemArr["id"]=$idName.$idCounter++;
+                $itemArr["id"]="node_".$idCounter++;
                 $itemArr["text"]=$item;
                 $itemArr["parent"]=$node["id"];
 
                 array_push($treeRoot, $itemArr);
-                findAllChild($treeRoot[count($treeRoot)-1], $currentPath, $idCounter, $idName, $treeRoot);
+                findAllChild($treeRoot[count($treeRoot)-1], $currentPath, $idCounter, $rootPath, $treeRoot);
             }
         }
         else {
             $node["a_attr"]=Array();
-            $node["a_attr"]["href"]=substr($currentPath, 3);
+            $node["a_attr"]["href"]="share".substr($currentPath, strlen($rootPath));
+
             $fileType=strtolower(pathinfo($currentPath, PATHINFO_EXTENSION));
+            
             $node["a_attr"]["fileType"]=$fileType;
             switch($fileType) {
                 case 'avi':
