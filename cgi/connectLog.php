0aa2f98997faaf04dd16bf6af2df5ed46b7ea522
Dauba <ab6060118@gmail.com>
2015-05-25 00:47:28 +0800

    modifiy feature: connect recorder add access file name

--- cgi/connectLog.php
+++ cgi/connectLog.php
@@ -1,7 +1,8 @@
 <?php
     date_default_timezone_set("Asia/Taipei");
     $logPath="/home/pi/web/910_cloud/connect.log";
-    $time=date("Y-m-d G:i:s");
+    $time=date("Y-m-d GG:i:s");
+    if(isset($_GET['fileName'])) $fileName=$_GET["fileName"];
 
     if (!empty($_SERVER['HTTP_CLIENT_IP']))
            $ip=$_SERVER['HTTP_CLIENT_IP'];
@@ -11,6 +12,9 @@
            $ip=$_SERVER['REMOTE_ADDR'];
 
     $log="[$time] Connect from $ip";
+
+    if(isset($fileName)) $log.=" access the file: $fileName";
+
     $command="echo \"$log\" >> \"$logPath\"";
     echo shell_exec($command);
 ?>
