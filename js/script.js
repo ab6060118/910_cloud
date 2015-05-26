2bb4c9e764f0c7525bfe48e13f0086de409a32e1
Dauba <ab6060118@gmail.com>
2015-05-27 01:11:28 +0800

    separate JavsScript code from index.html to js/script.js

---
+++ js/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    $.get("cgi/connectLog.php", function() {});
+
+    $('#ajaxTree').jstree({
+        'core' : {
+            'data': {
+                "url": "./tree.json",
+                "dataType": "json"
+            }
+            
+        }
+    });
+
+    $('#rightContent').hide();
+
+    $(document).ajaxStart(function() {
+        $('body').addClass("loading"); 
+    });
+
+    $(document).ajaxStop(function() {
+        $('body').removeClass("loading"); 
+    });
+
+    $('#createTree').on('click', function() {
+        // $('#ajaxTree').jstree('destroy');
+        $.get('cgi/createTree.php', function() {
+            $('#ajaxTree').jstree('refresh');
+        });
+    });
+
+    $('#ajaxTree').on('hover_node.jstree', function(e, data) {
+        if(data.node.a_attr.fileType != undefined) {
+            var type=data.node.a_attr.fileType;
+            var link=data.node.a_attr.href;
+            switch(type) {
+                case 'jpg':
+                case 'png':
+                case 'gif':
+                    $('#rightContent').show();
+                    $('#contentImg').attr("src", link);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+
+    $('#ajaxTree').on('dehover_node.jstree', function(e, data) {
+            $('#rightContent').hide();
+    });
+
+    $('#ajaxTree').on('select_node.jstree', function(e, data) {
+        if(data.node.a_attr.fileType != undefined) {
+            var type=data.node.a_attr.fileType;
+            var link=data.node.a_attr.href;
+            var fileName=data.node.text;
+            switch(type) {
+                case 'jpg':
+                case 'png':
+                case 'gif':
+                    break;
+                default:
+                    $.get("cgi/connectLog.php", {"fileName":fileName}, function() {});
+                    window.open(link);
+                    break;
+            }
+        }
+    });
+});
+
